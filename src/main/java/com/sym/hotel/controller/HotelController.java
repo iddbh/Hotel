f23f7972f58ded302e32b6fc13339a6a10e67d73
brdhub <2432152240@qq.com>
2022-10-01 20:49:34 +0800

    添加controller

---
+++ src/main/java/com/sym/hotel/controller/HotelController.java
@@ -0,0 +1,24 @@
+package com.sym.hotel.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HotelController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello!";
+    }
+
+    @ResponseBody
+    @RequestMapping("/")
+    public String mainPage(){
+        return "This is the hotel";
+    }
+
+
+
+}
