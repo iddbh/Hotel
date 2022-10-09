0d660f7e3b2211683fd91cbba0ad78875a1da5c9
sym <1955435228@qq.om>
2022-10-09 21:21:21 +0800

    Spring security除授权部分完成

---
+++ src/main/java/com/sym/hotel/Service/LoginService.java
@@ -0,0 +1,10 @@
+package com.sym.hotel.Service;
+
+import com.sym.hotel.domain.ResponseResult;
+import com.sym.hotel.pojo.Guest;
+
+public interface LoginService {
+    ResponseResult login(Guest guest);
+
+    ResponseResult logout();
+}
