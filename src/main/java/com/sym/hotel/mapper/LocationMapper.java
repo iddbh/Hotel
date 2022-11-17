95fe76e8407c8b500d8199ca065e95a4a0f59db1
brdhub <2432152240@qq.com>
2022-11-17 17:06:51 +0800

    City -> Location

---
+++ src/main/java/com/sym/hotel/mapper/LocationMapper.java
@@ -0,0 +1,8 @@
+package com.sym.hotel.mapper;
+
+import com.github.yulichang.base.MPJBaseMapper;
+import com.sym.hotel.pojo.Location;
+
+public interface LocationMapper extends MPJBaseMapper<Location> {
+
+}
