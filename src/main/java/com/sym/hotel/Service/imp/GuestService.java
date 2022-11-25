d39813cea44c17763ed2729d2152690a86d30187
YY <1418811259@qq.com>
2022-11-25 17:11:47 +0800

    补充

--- src/main/java/com/sym/hotel/Service/imp/GuestService.java
+++ src/main/java/com/sym/hotel/Service/imp/GuestService.java
@@ -1,12 +1,16 @@
 package com.sym.hotel.Service.imp;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.github.yulichang.wrapper.MPJLambdaWrapper;
 import com.sym.hotel.domain.LoginGuest;
 import com.sym.hotel.mapper.GuestMapper;
+import com.sym.hotel.mapper.RecordMapper;
 import com.sym.hotel.mapper.RoomMapper;
 import com.sym.hotel.mapper.TypeMapper;
 import com.sym.hotel.pojo.Guest;
+import com.sym.hotel.pojo.Record;
 import com.sym.hotel.pojo.Room;
 import com.sym.hotel.pojo.Type;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +18,12 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import com.sym.hotel.domain.ResponseResult;
 
+
+
+
+import javax.xml.crypto.Data;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -28,7 +37,9 @@ public class GuestService implements UserDetailsService {
     private RoomMapper roomMapper;
     @Autowired
     private TypeMapper typeMapper;
-
+    @Autowired
+    private RecordMapper recordMapper;
+//    private UserinfoMapper userinfoMapper;
 
 
     @Override
@@ -49,8 +60,22 @@ public class GuestService implements UserDetailsService {
 
     public List<Room> selectRoomByPrice(Double minMoney,Double maxMoney){
         List<Room> list=roomMapper.selectJoinList(Room.class,
-                new MPJLambdaWrapper<Room>().selectAll(Room.class).select(Type::getPrice)
-                        .leftJoin(Type.class,Type::getId,Room::getRoomTypeId).gt(Type::getPrice,minMoney).le(Type::getPrice,maxMoney));
+                new MPJLambdaWrapper<Room>().selectAll(Room.class).leftJoin(Type.class,Type::getId,Room::getRoomTypeId).select(Type::getPrice)
+                        .gt(Type::getPrice,minMoney).le(Type::getPrice,maxMoney));
         return list;
+    }
+    public ResponseResult bookRoom(Record recordNew){
+       Record record=null ;
+       record=recordMapper.selectOne(
+                new MPJLambdaWrapper<Record>().selectAll(Record.class)
+                        .lt(Record::getBookStartTime,recordNew.getBookEndTime()).ge(Record::getBookEndTime,recordNew.getBookEndTime())
+                        .or().le(Record::getBookStartTime,recordNew.getBookStartTime()).gt(Record::getBookEndTime,recordNew.getBookStartTime()));
+       if(record==null){
+//           int count = userMapper.insert(userEntity);
+           return new ResponseResult(200,"预定成功");
+       }else {
+           return new ResponseResult(200,"此房间该时段内已被预订，请您重新选择");
+       }
+
     }
 }
